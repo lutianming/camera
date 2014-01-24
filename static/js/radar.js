f101f51135b5439c7ca4df05210c86486733d3f8
Tianming.Lu <lutianming1005@gmail.com>
2014-01-24 16:21:58 +0100

    update radar.js

--- static/js/radar.js
+++ static/js/radar.js
@@ -1,37 +1,35 @@
 function show_radar(id,data,columns){
-  
-  var number_elements = data.length;
-  var number_columns = columns.length;
-  
-  var d  = [];
- 
-  
-  
-  for(var i=0; i<number_elements; i++){
-    var obj = [];
-    var tmp = data[i];
-    for(var j=0; j<number_columns; j++){
-      var col = colums[j]["field"];
-      obj.push({
-        axis: columns[j]["name"],
-        value: tmp[col]    
-        });
+
+    var number_elements = data.length;
+    var number_columns = columns.length;
+
+    var d  = [];
+
+
+
+    for(var i=0; i<number_elements; i++){
+	var obj = [];
+	var tmp = data[i];
+	for(var j=0; j<number_columns; j++){
+	    var col = colums[j]["field"];
+	    obj.push({
+		axis: columns[j]["name"],
+		value: tmp[col]
+            });
+	}
+	d.push(obj);
     }
-    d.push(obj);
-  }
-  
 
-}
-  
-  var mycfg = {
-  color: function(){
-  c = ['red', 'yellow', 'pink', 'green', 'blue', 'olive', 'aqua', 'cadetblue', 'crimson'];
-  m = c.length - 1;
-  x = parseInt(Math.random()*100);
-  return c[x%m]; //Get a random color
-}
-}
 
-RadarChart.draw(id, d, mycfg);
+var mycfg = {
+    color: function(){
+	c = ['red', 'yellow', 'pink', 'green', 'blue', 'olive', 'aqua', 'cadetblue', 'crimson'];
+	m = c.length - 1;
+	x = parseInt(Math.random()*100);
+	return c[x%m]; //Get a random color
+    }
+};
+
+    RadarChart.draw(id, d, mycfg);
 
 }
