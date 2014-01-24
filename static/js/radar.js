e7b739bc50ba0b3fa76a453b449e80126534988e
nathalipaz <nathali_paz@hotmail.com>
2014-01-24 16:16:47 +0100

    Update radar.js

--- static/js/radar.js
+++ static/js/radar.js
@@ -1,37 +1,27 @@
-function show_radar(data,columns){
+function show_radar(id,data,columns){
+  
+  var number_elements = data.length;
+  var number_columns = columns.length;
+  
+  var d  = [];
+ 
   
   
-  length; //objects
-  number_columns = columns.length;
-  number_elements = data.length;
-  
-  data = [];
   for(var i=0; i<number_elements; i++){
-    data.push([
-    	
-    	date: +tmp.release_date,
-                max_reso: +tmp.max_reso,
-                low_reso: +tmp.low_reso,
-                pixels: +tmp.pixels,
-                zoom_W: +tmp.zoom_W,
-                zoom_T: +tmp.zoom_T,
-                normal_focus: +tmp.normal_focus,
-                macro_focus: +tmp.macro_focus,
-                storage: +tmp.storage,
-                weight: +tmp.weight,
-                dimensions: +tmp.dimensions,
-                price: +tmp.price
-    	
-    	
-           {axis: "Max resolution", value: Math.floor((Math.random()*100)+1)}, 
-           {axis: "Low resolution", value: Math.floor((Math.random()*100)+1)}, 
-           {axis: "Piexels", value: Math.floor((Math.random()*100)+1)}, 
-           {axis: "Zoom W", value: Math.floor((Math.random()*100)+1)},
-           {axis: "Zoom T", value: Math.floor((Math.random()*100)+1)},  
-           {axis: "Normal Focus", value: Math.floor((Math.random()*100)+1)},  
-           {axis: "Macro Focus", value: Math.floor((Math.random()*100)+1)}
-          ])
+    var obj = [];
+    var tmp = data[i];
+    for(var j=0; j<number_columns; j++){
+      var col = colums[j]["field"];
+      obj.push({
+        axis: columns[j]["name"],
+        value: tmp[col]    
+        });
+    }
+    d.push(obj);
   }
+  
+
+}
   
   var mycfg = {
   color: function(){
@@ -41,6 +31,7 @@ function show_radar(data,columns){
   return c[x%m]; //Get a random color
 }
 }
-RadarChart.draw("#diagram", d, mycfg);
+
+RadarChart.draw(id, d, mycfg);
 
 }
